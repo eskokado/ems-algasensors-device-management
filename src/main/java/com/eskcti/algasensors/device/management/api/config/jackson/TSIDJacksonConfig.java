3c258a15045645b814f16a9d5b7e4c57521f4ebb
Edson Shideki Kokado <eskokado@gmail.com>
2026-06-19 11:30:39 -0300

    feat(device-management): update version jackson

--- src/main/java/com/eskcti/algasensors/device/management/api/config/jackson/TSIDJacksonConfig.java
+++ src/main/java/com/eskcti/algasensors/device/management/api/config/jackson/TSIDJacksonConfig.java
@@ -2,10 +2,8 @@ package com.eskcti.algasensors.device.management.api.config.jackson;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilderCustomizer;
-
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.module.SimpleModule;
+import tools.jackson.databind.JacksonModule;
+import tools.jackson.databind.module.SimpleModule;
 
 import io.hypersistence.tsid.TSID;
 
@@ -13,15 +11,9 @@ import io.hypersistence.tsid.TSID;
 public class TSIDJacksonConfig {
 
     @Bean
-    public Module tsidModule() {
+    public JacksonModule tsidModule() {
         SimpleModule module = new SimpleModule();
         module.addSerializer(TSID.class, new TSIDToStringSerializer());
-        module.addDeserializer(TSID.class, new TSIDToStringDeserializer());
-        return module;
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer tsidCustomizer() {
-        return builder -> builder.modulesToInstall(tsidModule());
+        return module; 
     }
 }
